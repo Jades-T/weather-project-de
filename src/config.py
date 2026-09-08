87db46fcf0b7d7cd8a122250cf0c665b3d999946
Jady-Ray Tamboer <jatamscc-g-025@student.wethinkcode.co.za>
2026-09-08 12:23:45 +0200

    feat: added the function that validates if the configuration settings works, exists and can be accessed in the environment

--- src/config.py
+++ src/config.py
@@ -15,39 +15,45 @@ def load_config() -> dict:
     #  Allows me to load the variables in the environment
 
     # config settings will be returned as a dictionary:
-    """
-    weather_api_key
-    weather_api_url
-    database_path
-    update_interval (Minutes)
-    maximum amount of retries (Integer)
-    retry_delay (Seconds)
-    log_level (Still need to find out what is cooking here?)
-    """
     config = {
         'weather_api_key': os.getenv('WEATHER_API_KEY', ''),
         'weather_api_url': os.getenv('WEATHER_API_URL','https://api.openweathermap.org/data/2.5/weather'),
         'database_path': os.getenv('DATABASE_PATH', 'data/weather.db'),
-        'update_interval': int(os.getenv('UPDATE_INTERVAL_MINUTES', '60')) ,        # minutes
+        'update_interval': int(os.getenv('UPDATE_INTERVAL', '60')) ,        # minutes
         'max_retries': int(os.getenv('MAX_RETRIES', '3')),
         'retry_delay': int(os.getenv('RETRY_DELAY', '1')),             # seconds
-        'log_level':,               # check what this does?
-        'log_file':    
+        # 'log_level':,               # check what this does?
+        # 'log_file':    
     }
 
     return config
 
 
 
-
-
-
-
-
-
 def validate_config(config: dict) -> bool:
-    pass
+    """
+    This functions validates that the configuration settings are correct and actually exists.
+    
+    Args: The dictionary config, will be used and also loaded using from load_config()
 
+    Returns: True if the configuration is valid and exists
+
+    Raises: Should give a ValueError if the config is invalid and does nort exist.
+    """
+
+    if not config.get('weather_api_key'):
+        raise ValueError("WEATHER_API_KEY is required to proceed.")
+    if not config.get('weather_api_url'):
+        raise ValueError("WEATHER_API_URL is missing. Add to configuration file.")
+    if not config.get('update_interval', 0) <=0:
+        raise ValueError("UPDATE_INTERNAL must be in minutes and must be positive.")
+    if not config.get('max_entries', 0) < 0:
+        raise ValueError("MAX ENTRIES cannot be negative.")
+    if not config.get('retry_delay'):
+        raise ValueError("RETRY_DELAY must be in seconds and must be positive.")
+
+    # See if you should add the log_levels information.
+    # Is the log level data needed?
 
 def get_config() -> dict:
     pass
